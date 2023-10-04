8d82df7a29b98e535ae627eaa94f347e1ac0e987
Ussama Azam <nuclearoreo@gmail.com>
2023-10-03 23:28:27 -0400

    Able to parse question

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
 use bytes::Bytes;
 use core::num;
-use dns_message_parser::{Dns, Flags};
+use dns_message_parser::{question, Dns, Flags};
 use std::num::ParseIntError;
+
+fn main() {
+    let ss = ["a01d81800001000100000000076578616d706c6503636f6d0000010001c00c0001000100001bbc00045db8d822",
+            "9b4c84000001000200000000037777770a636c6f7564666c61726503636f6d0000010001c00c000100010000012c000468107c60c00c000100010000012c000468107b60", 
+            "7ebd84000001000200000000037777770a636c6f7564666c61726503636f6d00001c0001c00c001c00010000012c001026064700000000000000000068107c60c00c001c00010000012c001026064700000000000000000068107b60",
+            "762081800001000200000000037777770773706f7469667903636f6d0000010001c00c0005000100000102001f12656467652d7765622d73706c69742d67656f096475616c2d67736c62c010c02d000100010000006c000423bae019",
+            "619381800001000100000000076578616d706c6503636f6d00001c0001c00c001c000100001bf9001026062800022000010248189325c81946", 
+            ];
+
+    for &s in ss.iter() {
+        let msg = decode_hex(s).unwrap();
+        parse_message(&msg);
+    }
+
+    // let s = "a01d81800001000100000000076578616d706c6503636f6d0000010001c00c0001000100001bbc00045db8d822";
+    // let s = "9b4c84000001000200000000037777770a636c6f7564666c61726503636f6d0000010001c00c000100010000012c000468107c60c00c000100010000012c000468107b60";
+    // let s = "7ebd84000001000200000000037777770a636c6f7564666c61726503636f6d00001c0001c00c001c00010000012c001026064700000000000000000068107c60c00c001c00010000012c001026064700000000000000000068107b60";
+    // let s = "762081800001000200000000037777770773706f7469667903636f6d0000010001c00c0005000100000102001f12656467652d7765622d73706c69742d67656f096475616c2d67736c62c010c02d000100010000006c000423bae019";
+    // let s = "619381800001000100000000076578616d706c6503636f6d00001c0001c00c001c000100001bf9001026062800022000010248189325c81946";
+    // for v in msg.iter() {
+    //     println!("{:x}", v);
+    // }
+
+    // let bytes = Bytes::copy_from_slice(&msg[..]);
+
+    // println!("{:?}", bytes);
+
+    // let dns = Dns::decode(bytes).unwrap();
+    // println!("{:?}", dns);
+}
 
 fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
     (0..s.len())
@@ -10,7 +40,85 @@ fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
         .collect()
 }
 
-fn parse_header(bytes: &[u8]) {
+fn parse_message(bytes: &[u8]) {
+    let (
+        id,
+        r_code,
+        zero_flag,
+        ra_flag,
+        rd_flag,
+        tc_flag,
+        aa_flag,
+        opcode,
+        qr_flag,
+        number_of_question,
+        number_of_answers_rr,
+        number_of_authority_rr,
+        number_of_additional_rr,
+    ) = parse_header(&bytes[0..12]);
+
+    parse_body(
+        number_of_question,
+        number_of_answers_rr,
+        number_of_authority_rr,
+        number_of_additional_rr,
+        &bytes[12..],
+    )
+}
+
+fn parse_body(
+    number_of_question: u16,
+    number_of_answers_rr: u16,
+    number_of_authority_rr: u16,
+    number_of_additional_rr: u16,
+    bytes: &[u8],
+) {
+    let mut questions = vec![];
+    let mut offset = 0;
+
+    // Iterate through the number of questions
+    for _ in 0..number_of_question {
+        // Grab Name
+        let (new_offset, name) = _get_name(offset, bytes);
+        offset = new_offset + 1;
+
+        // Get Question Type
+        let q_type = (bytes[offset] as u16) << 8 | (bytes[offset + 1] as u16);
+        offset += 2;
+
+        // Get Question Class
+        let q_class = (bytes[offset] as u16) << 8 | (bytes[offset + 1] as u16);
+        offset += 2;
+
+        // Append those results into a list
+        questions.push((name, q_type, q_class));
+    }
+
+    println!("{:?}\n", questions);
+
+    for _ in 0..number_of_answers_rr {}
+    for _ in 0..number_of_authority_rr {}
+    for _ in 0..number_of_additional_rr {}
+}
+
+fn _get_name(mut offset: usize, bytes: &[u8]) -> (usize, String) {
+    let mut strings = vec![];
+    while bytes[offset] != 0 {
+        let mut token = vec![];
+        for i in 0..bytes[offset] {
+            let byte = bytes[offset + i as usize + 1];
+            token.push(byte);
+        }
+        let string = String::from_utf8(token).unwrap();
+        strings.push(string);
+        offset += bytes[offset] as usize + 1;
+    }
+
+    let name = strings.join(".");
+    (offset, name)
+}
+
+fn parse_header(bytes: &[u8]) -> (u16, u8, u8, u8, u8, u8, u8, u8, u8, u16, u16, u16, u16) {
     // Mask to filter out
     let mask_4 = (1 << 4) as u16 - 1;
     let mask_3 = (1 << 3) as u16 - 1;
@@ -59,38 +167,26 @@ fn parse_header(bytes: &[u8]) {
     let number_of_additional_rr = (bytes[10] as u16) << 8 | (bytes[11] as u16);
 
     println!(
-        "Id: {}, rCode: {:04b}, zero: {:03b}, ra: {:01b}, rd: {:01b}, tc: {:01b}, aa: {:01b}, opcode: {:04b}, qr: {:01b}, 
-        number_of_question: {}, number_of_answers_rr: {}, number_of_authority_rr: {}, number_of_additional_rr: {} \n",
-        id, r_code, zero_flag, ra_flag, rd_flag, tc_flag, aa_flag, opcode, qr_flag, number_of_question, number_of_answers_rr, number_of_authority_rr, number_of_additional_rr 
+        "Id: {}, rCode: {:04b}, zero: {:03b}, ra: {:01b}, rd: {:01b}, tc: {:01b}, aa: {:01b}, opcode: {:04b}, qr: {:01b},
+        number_of_question: {}, number_of_answers_rr: {}, number_of_authority_rr: {}, number_of_additional_rr: {}",
+        id, r_code, zero_flag, ra_flag, rd_flag, tc_flag, aa_flag, opcode, qr_flag, number_of_question, number_of_answers_rr, number_of_authority_rr, number_of_additional_rr
     );
-}
 
-fn main() {
-    let ss = ["a01d81800001000100000000076578616d706c6503636f6d0000010001c00c0001000100001bbc00045db8d822",
-            "9b4c84000001000200000000037777770a636c6f7564666c61726503636f6d0000010001c00c000100010000012c000468107c60c00c000100010000012c000468107b60", 
-            "7ebd84000001000200000000037777770a636c6f7564666c61726503636f6d00001c0001c00c001c00010000012c001026064700000000000000000068107c60c00c001c00010000012c001026064700000000000000000068107b60",
-            "762081800001000200000000037777770773706f7469667903636f6d0000010001c00c0005000100000102001f12656467652d7765622d73706c69742d67656f096475616c2d67736c62c010c02d000100010000006c000423bae019",
-            "619381800001000100000000076578616d706c6503636f6d00001c0001c00c001c000100001bf9001026062800022000010248189325c81946", 
-            ];
+    let tuple = (
+        id,
+        r_code,
+        zero_flag,
+        ra_flag,
+        rd_flag,
+        tc_flag,
+        aa_flag,
+        opcode,
+        qr_flag,
+        number_of_question,
+        number_of_answers_rr,
+        number_of_authority_rr,
+        number_of_additional_rr,
+    );
 
-    for &s in ss.iter() {
-        let msg = decode_hex(s).unwrap();
-        parse_header(&msg[0..12]);
-    }
-
-    // let s = "a01d81800001000100000000076578616d706c6503636f6d0000010001c00c0001000100001bbc00045db8d822";
-    // let s = "9b4c84000001000200000000037777770a636c6f7564666c61726503636f6d0000010001c00c000100010000012c000468107c60c00c000100010000012c000468107b60";
-    // let s = "7ebd84000001000200000000037777770a636c6f7564666c61726503636f6d00001c0001c00c001c00010000012c001026064700000000000000000068107c60c00c001c00010000012c001026064700000000000000000068107b60";
-    // let s = "762081800001000200000000037777770773706f7469667903636f6d0000010001c00c0005000100000102001f12656467652d7765622d73706c69742d67656f096475616c2d67736c62c010c02d000100010000006c000423bae019";
-    // let s = "619381800001000100000000076578616d706c6503636f6d00001c0001c00c001c000100001bf9001026062800022000010248189325c81946";
-    // for v in msg.iter() {
-    //     println!("{:x}", v);
-    // }
-
-    // let bytes = Bytes::copy_from_slice(&msg[..]);
-
-    // println!("{:?}", bytes);
-
-    // let dns = Dns::decode(bytes).unwrap();
-    // println!("{:?}", dns);
+    tuple
 }
